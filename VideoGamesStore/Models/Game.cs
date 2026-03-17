fdd9be4c9bdb258fab9ceb5106f43606ec21cc91
sakoji88 <slonionok0407@gmail.com>
2026-03-17 10:24:34 +0300

    Усилить валидацию полей и русифицировать подписи

--- VideoGamesStore/Models/Game.cs
+++ VideoGamesStore/Models/Game.cs
@@ -8,39 +8,51 @@ public partial class Game
     [Key]
     public int Id { get; set; }
 
+    [Display(Name = "ID в RAWG")]
     public int? RawgId { get; set; }
 
+    [Display(Name = "Название")]
     [Required(ErrorMessage = "Укажите название игры.")]
     [StringLength(200, MinimumLength = 2, ErrorMessage = "Название должно содержать от 2 до 200 символов.")]
     public string Title { get; set; } = null!;
 
+    [Display(Name = "Описание")]
     [StringLength(4000, ErrorMessage = "Описание не должно быть длиннее 4000 символов.")]
     public string? Description { get; set; }
 
+    [Display(Name = "Дата выхода")]
     public DateOnly? ReleaseDate { get; set; }
 
+    [Display(Name = "Цена")]
     [Range(typeof(decimal), "0,01", "999999,99", ErrorMessage = "Цена должна быть в диапазоне от 0,01 до 999999,99.")]
     [Column(TypeName = "decimal(10, 2)")]
     public decimal Price { get; set; }
 
+    [Display(Name = "Остаток")]
     [Range(0, 100000, ErrorMessage = "Количество на складе должно быть от 0 до 100000.")]
     public int Stock { get; set; }
 
+    [Display(Name = "Рейтинг")]
     [Range(typeof(decimal), "0", "10", ErrorMessage = "Рейтинг должен быть в диапазоне от 0 до 10.")]
     [Column(TypeName = "decimal(3, 1)")]
     public decimal? Rating { get; set; }
 
+    [Display(Name = "Ссылка на обложку")]
     [StringLength(500, ErrorMessage = "Ссылка на обложку не должна быть длиннее 500 символов.")]
     public string? CoverImageUrl { get; set; }
 
+    [Display(Name = "Возрастной рейтинг")]
     [StringLength(20, ErrorMessage = "Возрастной рейтинг не должен быть длиннее 20 символов.")]
     public string? AgeRating { get; set; }
 
+    [Display(Name = "Издатель")]
     public int? PublisherId { get; set; }
 
+    [Display(Name = "Жанр")]
     [Range(1, int.MaxValue, ErrorMessage = "Выберите жанр.")]
     public int GenreId { get; set; }
 
+    [Display(Name = "Активна")]
     public bool IsActive { get; set; }
 
     [Column(TypeName = "datetime")]
--- VideoGamesStore/Models/Genre.cs
+++ VideoGamesStore/Models/Genre.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -12,9 +12,9 @@ public partial class Genre
     [Key]
     public int Id { get; set; }
 
-
-
-    [StringLength(100)]
+    [Display(Name = "Название жанра")]
+    [Required(ErrorMessage = "Укажите название жанра.")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "Название жанра должно содержать от 2 до 100 символов.")]
     public string Name { get; set; } = null!;
 
     [InverseProperty("Genre")]
